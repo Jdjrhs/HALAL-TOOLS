1e68abdbf38ab0f0d11cc6c147ec33d9a43efdae
Hehew.hac <116163637+Jdjrhs@users.noreply.github.com>
2022-10-21 11:45:44 +0700

    HALAL TOOLS
    IN THESE TOOLS THERE IS AL-FATIHAH AND EXIT AND THAT'S WHAT MAKES ME GIVE THE NAMES OF THESE TOOLS HALAL TOOLS THERE WILL BE OTHER LETTER, WHY KAYA AN-NAAS AND OTHERS  BUT THE CODE WILL BE WAITING FOR A FULL UPDATE :)

---
+++ juzama.sh
@@ -0,0 +1,86 @@
+#!/bin/usr/bash
+
+#et mau ngapain?
+#mau copy pas ya?
+#ok boleh
+#sambil sambil perbanya tools halal
+#mari kita menuju jalah yabg benar:)
+
+clear
+#variable
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+
+
+toilet -f big -F gay juzama
+echo
+echo -e $blue "========================"
+echo       "      WELCOME TO MY JUZ    "
+echo       "  {•}AUTHOR:Jdjrhs         "
+echo       "  {•}YOUTBE:KAISUNETS ??   "
+echo          "========================"
+echo -e $g "    SILAHKAN PILIH      "
+echo       "  {•}AL-fatihah            "
+echo       "  {•}exit                  "
+echo
+read -p "PILIHAN : " pil;
+if [ $pil = 1 ] || [ $pil = 01 ]
+then
+clear
+
+sleep 1
+
+figlet AL-FATIHAH
+
+echo -e $red "DIHARAP KAN MENGGUNAKAN LAYAR YG SEUSAI SAJA"
+
+sleep 2
+
+echo -e $g "بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ"
+echo "1. Dengan nama Allah Yang Maha Pengasih, Maha Penyayang."
+
+echo "اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَۙ "
+echo "2. Segala puji bagi Allah, Tuhan seluruh alam,"
+
+echo "الرَّحْمٰنِ الرَّحِيْمِۙ "
+echo "3. Yang Maha Pengasih, Maha Penyayang,"
+
+echo "مٰلِكِ يَوْمِ الدِّيْنِۗ "
+echo "4. Pemilik hari pembalasan."
+
+echo "اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُۗ"
+echo "5. Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami mohon pertolongan."
+
+echo  "اِهْدِنَا الصِّرَاطَ الْمُسْتَقِيْمَ ۙ "
+echo  "6. Tunjukilah kami jalan yang lurus,"
+
+echo "صِرَاطَ الَّذِيْنَ اَنْعَمْتَ عَلَيْهِمْ ەۙ غَيْرِ الْمَغْضُوْبِ عَلَيْهِمْ وَلَا الضَّاۤلِّيْنَ ࣖ "
+echo "7. yaitu jalan orang-orang yang telah Engkau beri nikmat kepadanya bukan jalan mereka yang dimurkai, dan bukan pula jalan mereka yang sesat."
+fi
+if [ $pil = 2 ] || [ $pil = 02 ]
+then
+
+clear
+
+figlet BYE
+echo "BYE BANG"
+
+sleep 2
+
+exit
+fi
+
+
